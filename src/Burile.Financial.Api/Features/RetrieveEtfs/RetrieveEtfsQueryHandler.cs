30b2afa12a101ebda3c515fc4ba09bd16b7282b6
Cristian Burile <cristianburile@hotmail.com>
2023-11-15 10:33:39 -0300

    refactor: rename RetrieveEtfsHandler to RetrieveEtfsQueryHandler
    Signed-off-by: Cristian Burile <cristianburile@hotmail.com>

--- src/Burile.Financial.Api/Features/RetrieveEtfs/RetrieveEtfsQueryHandler.cs
+++ src/Burile.Financial.Api/Features/RetrieveEtfs/RetrieveEtfsQueryHandler.cs
@@ -3,11 +3,11 @@ using MediatR;
 
 namespace Burile.Financial.Api.Features.RetrieveEtfs;
 
-public sealed class RetrieveEtfsHandler : IRequestHandler<RetrieveEtfsQuery, string>
+public sealed class RetrieveEtfsQueryHandler : IRequestHandler<RetrieveEtfsQuery, string>
 {
     private readonly ITwelveDataClient _twelveDataClient;
 
-    public RetrieveEtfsHandler(ITwelveDataClient twelveDataClient)
+    public RetrieveEtfsQueryHandler(ITwelveDataClient twelveDataClient)
         => _twelveDataClient = twelveDataClient;
 
     public async Task<string> Handle(RetrieveEtfsQuery request, CancellationToken cancellationToken = default)
