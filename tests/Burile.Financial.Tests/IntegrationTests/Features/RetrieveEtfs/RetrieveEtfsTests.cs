89ba741f5250af0821ab0207c646244f6440921b
Cristian Burile <cristianburile@hotmail.com>
2023-11-15 10:36:34 -0300

    test: Create integration test RetrieveEtfsTests
    Signed-off-by: Cristian Burile <cristianburile@hotmail.com>

---
+++ tests/Burile.Financial.Tests/IntegrationTests/Features/RetrieveEtfs/RetrieveEtfsTests.cs
@@ -0,0 +1,32 @@
+using System.Net;
+using Burile.Financial.Tests.IntegrationTests.Features.RetrieveEtfs.Fixtures;
+using FluentAssertions;
+using Xunit;
+
+namespace Burile.Financial.Tests.IntegrationTests.Features.RetrieveEtfs;
+
+[Collection("RetrieveEtfsBackgroundServicesTest")]
+public sealed class RetrieveEtfsTests
+{
+    private readonly RetrieveEtfsBackgroundServicesFixture _retrieveEtfsBackgroundServicesFixture;
+
+    public RetrieveEtfsTests(RetrieveEtfsBackgroundServicesFixture retrieveEtfsBackgroundServicesFixture)
+        => _retrieveEtfsBackgroundServicesFixture = retrieveEtfsBackgroundServicesFixture;
+
+    [Fact]
+    public async Task RetrieveEtfs()
+    {
+        //Arrange
+
+        //Act
+        var responseMessage = await _retrieveEtfsBackgroundServicesFixture.HttpClient.GetAsync("api/etfs");
+
+        var content = await responseMessage.Content.ReadAsStringAsync();
+
+        //Assert
+        responseMessage.StatusCode.Should().Be(HttpStatusCode.OK);
+        responseMessage.IsSuccessStatusCode.Should().BeTrue();
+        content.Should()
+               .Contain("{\"symbol\":\"VT\",\"name\":\"Vanguard Total World Stock Index Fund ETF Shares\",\"currency\":\"USD\",\"exchange\":\"NYSE\",\"mic_code\":\"ARCX\",\"country\":\"United States\"}");
+    }
+}
