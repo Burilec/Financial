0b5226225303d80fa6bca664bd7bbe26bc3c9e6e
Cristian Burile <cristianburile@hotmail.com>
2023-11-15 10:32:12 -0300

    test: Create RetrieveEtfsBackgroundServicesFixture
    Signed-off-by: Cristian Burile <cristianburile@hotmail.com>

---
+++ tests/Burile.Financial.Tests/IntegrationTests/Features/RetrieveEtfs/Fixtures/RetrieveEtfsBackgroundServicesFixture.cs
@@ -0,0 +1,37 @@
+using Burile.Financial.Api;
+using Burile.Financial.Tests.IntegrationTests.Base;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Xunit;
+using static Microsoft.Extensions.Hosting.Host;
+
+namespace Burile.Financial.Tests.IntegrationTests.Features.RetrieveEtfs.Fixtures;
+
+[CollectionDefinition("RetrieveEtfsBackgroundServicesTest")]
+public class BackgroundServicesCollection : ICollectionFixture<RetrieveEtfsBackgroundServicesFixture>
+{
+}
+
+public sealed class RetrieveEtfsBackgroundServicesFixture : WebInfrastructureFixtureBase
+{
+    private const string HostEnvironment = "Local";
+
+    protected override IHostBuilder CreatedHostBuilder()
+    {
+        var hostBuilder = CreateDefaultBuilder(Array.Empty<string>())
+                         .UseEnvironment(HostEnvironment)
+                         .ConfigureHostConfiguration(ConfigureHostConfiguration())
+                         .ConfigureWebHostDefaults(static builder => builder.UseTestServer().BaseApiConfiguration());
+
+        return hostBuilder;
+    }
+
+    private static Action<IConfigurationBuilder> ConfigureHostConfiguration()
+        => static builder => builder.SetBasePath(Environment.CurrentDirectory)
+                                    .AddEnvironmentVariables();
+
+    protected override void Initialize()
+    {
+    }
+}
